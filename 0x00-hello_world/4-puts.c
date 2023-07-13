8804b41120d4b485c804b791ccec02495641acf2
Fa-Sil <fasil1284@gmail.com>
2023-07-13 08:15:13 -0700

    Adding C files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-int main(void)
-{
-	/*
+	/**
 	 *main - Entry point
 	 *
 	 *Description: A function that prints Programming is like building a multilingual puzzle
 	 *@parameter: No parameters
 	 *
 	 * Return: Always 0 (Success)
-	 * \
+	 * 
+	 */
+int main(void) 
 	puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
