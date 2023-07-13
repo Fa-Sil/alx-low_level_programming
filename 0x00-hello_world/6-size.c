d7d4f85d82c08e41e2746f894b95072447bd2c74
Fa-Sil <fasil1284@gmail.com>
2023-07-13 09:31:05 -0700

    Adding C files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,9 @@
  */
 int main(void)
 {
-	printf("Size of int is, sizeof(int)");
+	printf("Size of a char:", sizeof(char));
+	printf("Size of int:", sizeof(int));
+	printf("Size of a long int:", sizeof(long int));
+	printf("Size of a float:", sizeof(float));
 	return (0);
 }
