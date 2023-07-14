fe402e80085a7b1a60773269b8f9245ca7613aa4
Fa-Sil <fasil1284@gmail.com>
2023-07-14 11:44:36 -0700

    Adding c file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ *
+ * Return: Always 0 (Sucess)
  */
+
 int main(void)
 {
-	int n;
-	char l;
+	char c;
 
-	for (l = 'a'; l <= 'z'; l++)
+	for (c = 'a' ; c <= 'z' ; c++)
 	{
-		putchar(l);
-		putchar('\n');
+		putchar(c);
 	}
+	putchar('\n');
 	return (0);
 }
+
