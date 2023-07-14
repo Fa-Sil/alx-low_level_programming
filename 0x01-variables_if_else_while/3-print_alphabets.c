4d80c2399699ec7de4d582729328f35a83620c89
Fa-Sil <fasil1284@gmail.com>
2023-07-14 12:14:17 -0700

    adding c files

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	for (x = 'A' ; x <= 'Z' ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
+
