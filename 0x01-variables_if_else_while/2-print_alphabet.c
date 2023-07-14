91adec238c822f2e563c3599ea1e7e824ed08a15
Fa-Sil <fasil1284@gmail.com>
2023-07-14 04:38:59 -0700

    Adding the c files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char l = a;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	while (l <= z)
+	{
+		putchar(l);
+		putchar('\n');
+		++l;
+	}
+	return (0);
+}
