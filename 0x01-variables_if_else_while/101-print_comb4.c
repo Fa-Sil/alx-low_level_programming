cca38de718603c1411c43a979bc64c4a6022c53b
Fa-Sil <fasil1284@gmail.com>
2023-07-15 07:21:56 -0700

    Adding Additional c files

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 10; j++)
+			if (i > j)
+				break;
+			for (k = 0; k <= 10; k++)
+			{
+				if (j > k)
+					break;
+				printf("%d%d%d", i, j, k);
+			}
+	}
+	return (0);
+}
