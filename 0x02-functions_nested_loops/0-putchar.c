be4247431a762d729272a0af0bcc2d67983989d9
Fa-Sil <fasil1284@gmail.com>
2023-07-19 13:46:37 -0700

    updating the c files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,14 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int main (void)
 {
-	char *c = "_putchar\n";
-	
-	while (*c)
-	{
-		_putchar(*c);
-		c++;
-	}
+	char *c = "_putchar\n"
 
+		while (*c)
+		{
+			_putchar(*c);
+			c++;
+		}
 	return (0);
 }
