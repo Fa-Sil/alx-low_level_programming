48a14aaed59db587a1dba51e89b0b11f531b5385
Fa-Sil <fasil1284@gmail.com>
2023-07-21 12:37:39 -0700

    Adding the first C file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
-/*
- * _isupper(c) - returns 1 if the passed letter is 1
+/**
+ * _isupper - returns 1 if the passed letter is capital
+ * @c: character to be checked
  *
  * Return: 1 if letter is upper and 0 otherwise
  *
@@ -8,7 +9,7 @@
 
 int _isupper(c)
 {
-	if(c > 'A' && c <= 'Z')
+	if (c > 'A' && c <= 'Z')
 	{
 		return (1);
 	}
