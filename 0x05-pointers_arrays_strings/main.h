84aad458aec1f6a601bfe0d1528343be4f29484e
Fa-Sil <fasil1284@gmail.com>
2023-07-25 13:58:19 -0700

    Adding c files

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -12,3 +14,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
+#endif
