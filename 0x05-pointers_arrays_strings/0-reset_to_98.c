9e3918ddd89d10e07b88d1eacf76347db1764a6d
Fa-Sil <fasil1284@gmail.com>
2023-07-25 07:49:21 -0700

    Adding Pointer files

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
-#include<stdlib.h>
-/* main - update value to 98
- *
+#include "main.h"
+/**
+ * reset_to_98 - update value to 98
+ * @n: pointer parameter to be changed to 98
  * Return: 98
  */
 void reset_to_98(int *n)
