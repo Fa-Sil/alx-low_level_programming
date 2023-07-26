546224de31e43778e8ceb16f86091560c27d8bfe
Fa-Sil <fasil1284@gmail.com>
2023-07-26 12:35:30 -0700

    Adding the first c file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,8 +8,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char str[];
-
-	str = strcat(dest, src);
-	return (*str);
+int i, j;
+i = j = 0;
+while (*(dest + i))
+i++;
+while ((*(dest + i) = *(src + j)))
+{
+i++;
+j++;
+}
+return (dest);
 }
