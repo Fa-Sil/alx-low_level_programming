69bf4763902390a48a613c1a9fe9dd4dc3cf9dad
Fa-Sil <fasil1284@gmail.com>
2023-08-07 10:11:51 -0700

    Updating the second file

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,20 +7,25 @@
  */
 char *_strdup(char *str)
 {
-	int i;
-	char *s;
+	char *dup;
+	unsigned int i, len;
 
-	if (str == 0)
-		return (0);
-	for (i = 0; str[i]; i++)
-		;
-	s = malloc(sizeof(char) * (i + 1));
-	if (s == 0)
-		return (0);
-	s[i] = '\0';
-	while (i--)
-	{
-		s[i] = str[i];
-	}
-	return (s);
+	i = 0;
+	len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	dup = malloc(sizeof(char) * (len + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	while ((dup[i] = str[i]) != '\0')
+		i++;
+
+	return (dup);
 }
